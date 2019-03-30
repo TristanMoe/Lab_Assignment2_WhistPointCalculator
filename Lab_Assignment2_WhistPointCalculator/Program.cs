b37e6b347b101da546a6b60cbd2d3e19436178ed
TristanMoe <Trilleo@hotmail.com>
2019-03-30 17:32:13 +0100

    Add project files.

---
+++ Lab_Assignment2_WhistPointCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_Assignment2_WhistPointCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
